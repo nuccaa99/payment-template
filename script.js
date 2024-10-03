569b46eb4abd83c54539943e1d312a2aa9b2cffb
Nino Kharazishvili <ninokharazishvili@credo.ge>
2024-10-03 18:06:04 +0400

    added sec step styles

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
+// variables to change on second step
 const moreInfoSection = document.querySelector(".more_info_wrapper");
 const ibanWrapper = document.querySelector(".iban_wrapper");
 const merchantLocation = document.querySelector(".merchant_location");
+const cardInfoForm = document.querySelector(".card_info_form");
+const codeForm = document.querySelector(".code_form");
 
 const showSecStep = (e) => {
   e.preventDefault();
   // hide more info section in the header on second step
   moreInfoSection.style.display = "none";
+  // display iban on second step
   ibanWrapper.style.display = "flex";
+  // hide merchant location on second step
   merchantLocation.style.display = "none";
+  // hide card info form on second step
+  cardInfoForm.style.display = "none";
+  // display code form on second step
+  codeForm.style.display = "flex";
+};
+
+// variables to change on third step
+
+const merchantAndPriceContainer = document.querySelector(
+  ".merchant_price_container"
+);
+const successCardContainer = document.querySelector(".success_card_container");
+
+const showThirdStep = (e) => {
+  e.preventDefault();
+  merchantAndPriceContainer.style.display = "none";
+  codeForm.style.display = "none";
+  successCardContainer.style.display = "flex";
 };
