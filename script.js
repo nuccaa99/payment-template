cf0c7b63b5578ad69e16ea17839ce266a38f6317
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-10-08 22:22:38 +0400

    added final functionalities

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // close window
-const modal = document.querySelector(".modal_contents_container");
+const modal = document.querySelector('.modal_contents_container');
 function handleClose() {
-  modal.classList.add("hidden");
+  modal.classList.add('hidden');
 }
 
 // price amount format with 2 zeros
@@ -11,17 +11,17 @@ const formatToFixed = (selector) => {
   element.textContent = parseFloat(element.textContent).toFixed(2);
 };
 
-formatToFixed(".amount");
-formatToFixed(".btn_amount");
-formatToFixed(".check_amount");
+formatToFixed('.amount');
+formatToFixed('.btn_amount');
+formatToFixed('.check_amount');
 
 // Determine card type
 function getCardType(cardNumber) {
   let cardType = null;
   if (/^4/.test(cardNumber)) {
-    cardType = "Visa";
+    cardType = 'Visa';
   } else if (/^5[1-5]/.test(cardNumber)) {
-    cardType = "MasterCard";
+    cardType = 'MasterCard';
   }
   return cardType;
 }
@@ -44,52 +44,52 @@ function isValidCardNumber(cardNumber) {
 }
 
 // card number (pan) input format and visa/mc icon
-const visaIcon = document.getElementById("pan-icon-visa");
-const mcIcon = document.getElementById("pan-icon-mc");
+const visaIcon = document.getElementById('pan-icon-visa');
+const mcIcon = document.getElementById('pan-icon-mc');
 
 function handleCardNumber(e) {
   const input = e.target;
   input.value = input.value
-    .replace(/\D/g, "")
-    .replace(/(\d{4})(?=\d)/g, "$1 ")
+    .replace(/\D/g, '')
+    .replace(/(\d{4})(?=\d)/g, '$1 ')
     .trim();
-  const cardType = getCardType(input.value.replace(/\s/g, ""));
+  const cardType = getCardType(input.value.replace(/\s/g, ''));
 
-  visaIcon.classList.toggle("hidden", cardType !== "Visa");
-  mcIcon.classList.toggle("hidden", cardType !== "MasterCard");
+  visaIcon.classList.toggle('hidden', cardType !== 'Visa');
+  mcIcon.classList.toggle('hidden', cardType !== 'MasterCard');
 
   updateButtonState();
 }
 
-const cardNumberInput = document.getElementById("pan");
+const cardNumberInput = document.getElementById('pan');
 const cardNumberInputWrapper = document.querySelector(
-  ".card_info_input_wrapper"
+  '.card_info_input_wrapper'
 );
 
 // validate card number on blur event
-cardNumberInput.addEventListener("blur", function () {
-  const cardNumber = cardNumberInput.value.replace(/\s/g, "");
+cardNumberInput.addEventListener('blur', function () {
+  const cardNumber = cardNumberInput.value.replace(/\s/g, '');
   // Check if card number is 16 digits and passes Luhn algorithm
   if (!isValidCardNumber(cardNumber)) {
-    cardNumberInputWrapper.classList.add("validation_error");
+    cardNumberInputWrapper.classList.add('validation_error');
   } else {
-    cardNumberInputWrapper.classList.remove("validation_error");
+    cardNumberInputWrapper.classList.remove('validation_error');
   }
 });
 
 // expiry date input format
 
 function handleExpDate(e) {
-  let input = e.target.value.replace(/\D/g, "");
+  let input = e.target.value.replace(/\D/g, '');
   if (
     (input.length === 1 && input[0] > 1 && input[0] <= 9) ||
     (input.length === 2 && input[0] == 1 && input[1] > 2)
   ) {
-    input = "0" + input;
+    input = '0' + input;
   }
 
   if (input.length > 2) {
-    input = input.slice(0, 2) + " / " + input.slice(2, 4);
+    input = input.slice(0, 2) + ' / ' + input.slice(2, 4);
   }
 
   e.target.value = input.slice(0, 7);
@@ -99,7 +99,7 @@ function handleExpDate(e) {
 
 // Function to validate expiration date (MM/YY)
 function isValidExpirationDate(expDate) {
-  const [month, year] = expDate.split("/").map(Number);
+  const [month, year] = expDate.split('/').map(Number);
 
   if (!month || !year || month < 1 || month > 12) return false;
 
@@ -113,41 +113,41 @@ function isValidExpirationDate(expDate) {
 }
 
 // Validate expiration date on blur
-const expDateInput = document.getElementById("expDate");
-const expDateInputWrapper = document.getElementById("expDateWrapper");
+const expDateInput = document.getElementById('expDate');
+const expDateInputWrapper = document.getElementById('expDateWrapper');
 
-expDateInput.addEventListener("blur", function () {
+expDateInput.addEventListener('blur', function () {
   const expDate = expDateInput.value;
   if (!isValidExpirationDate(expDate)) {
-    expDateInputWrapper.classList.add("validation_error");
+    expDateInputWrapper.classList.add('validation_error');
   } else {
-    expDateInputWrapper.classList.remove("validation_error");
+    expDateInputWrapper.classList.remove('validation_error');
   }
 });
 
 // CVC/CVV input format
 
 function handleCvv(e) {
-  let input = e.target.value.replace(/\D/g, "");
+  let input = e.target.value.replace(/\D/g, '');
   e.target.value = input.slice(0, 3);
 
   updateButtonState();
 }
 
-const cvvInput = document.getElementById("cardCvv");
-const cvvInputWrapper = document.getElementById("cvvWrapper");
+const cvvInput = document.getElementById('cardCvv');
+const cvvInputWrapper = document.getElementById('cvvWrapper');
 
-cvvInput.addEventListener("blur", function () {
+cvvInput.addEventListener('blur', function () {
   const cvv = cvvInput.value;
   if (cvv.length !== 3) {
-    cvvInputWrapper.classList.add("validation_error");
+    cvvInputWrapper.classList.add('validation_error');
   } else {
-    cvvInputWrapper.classList.remove("validation_error");
+    cvvInputWrapper.classList.remove('validation_error');
   }
 });
 
 function updateButtonState() {
-  const cardNumber = cardNumberInput.value.replace(/\s/g, "");
+  const cardNumber = cardNumberInput.value.replace(/\s/g, '');
   const expDate = expDateInput.value;
   const cvv = cvvInput.value;
 
@@ -156,45 +156,60 @@ function updateButtonState() {
     isValidExpirationDate(expDate) &&
     cvv.length === 3;
 
-  const submitButton = document.getElementById("cardInfoSubmitBtn");
-  submitButton.classList.toggle("invalid", !allValid);
-  submitButton.disabled = false;
+  const submitButton = document.getElementById('cardInfoSubmitBtn');
+  submitButton.classList.toggle('invalid', !allValid);
+  allValid ? (submitButton.disabled = false) : (submitButton.disabled = true);
 }
 
 // mask primary account number aka pan on the second step
-const panElement = document.querySelector(".pan");
+const panElement = document.querySelector('.pan');
 const maskedPan =
-  "*".repeat(12) + panElement.textContent.replace(/\s+/g, "").slice(12);
+  '*'.repeat(12) + panElement.textContent.replace(/\s+/g, '').slice(12);
 panElement.textContent = maskedPan;
 
 // variables to change on second step
-const panWrapper = document.querySelector(".pan_wrapper");
-const merchantLocation = document.querySelector(".merchant_location");
-const cardInfoForm = document.querySelector(".card_info_form");
-const codeForm = document.querySelector(".code_form");
+
+const panWrapper = document.querySelector('.pan_wrapper');
+const merchantLocation = document.querySelector('.merchant_location');
+const cardInfoForm = document.querySelector('.card_info_form');
+const codeForm = document.querySelector('.code_form');
+
+const codeInput = document.getElementById('code');
+
+function handleCode(e) {
+  let input = e.target.value.replace(/\D/g, '');
+  e.target.value = input.slice(0, 7);
+  const submitButton = document.getElementById('codeSubmitBtn');
+  const isCodeValid = e.target.value.length === 6;
+
+  submitButton.classList.toggle('invalid', !isCodeValid);
+  isCodeValid
+    ? (submitButton.disabled = false)
+    : (submitButton.disabled = true);
+}
 
 const showSecStep = (e) => {
   e.preventDefault();
   // display pan on second step
-  panWrapper.style.display = "flex";
+  panWrapper.style.display = 'flex';
   // hide merchant location on second step
-  merchantLocation.style.display = "none";
+  merchantLocation.style.display = 'none';
   // hide card info form on second step
-  cardInfoForm.style.display = "none";
+  cardInfoForm.style.display = 'none';
   // display code form on second step
-  codeForm.style.display = "flex";
+  codeForm.style.display = 'flex';
 };
 
 // variables to change on third step
 
 const merchantAndAmountContainer = document.querySelector(
-  ".merchant_amount_container"
+  '.merchant_amount_container'
 );
-const successCardContainer = document.querySelector(".success_card_container");
+const successCardContainer = document.querySelector('.success_card_container');
 
 const showThirdStep = (e) => {
   e.preventDefault();
-  merchantAndAmountContainer.style.display = "none";
-  codeForm.style.display = "none";
-  successCardContainer.style.display = "flex";
+  merchantAndAmountContainer.style.display = 'none';
+  codeForm.style.display = 'none';
+  successCardContainer.style.display = 'flex';
 };
