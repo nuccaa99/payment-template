7ab84de33383d5cfff6bf530b489e1531dda17d8
Nino (Nutsa) Kharazishvili <nutsa1999@gmail.com>
2024-10-09 22:51:22 +0400

    final fixes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // get DOM elements
-const modal = document.querySelector(".modal_contents_container");
+const modal = document.querySelector('.modal_contents_container');
 
-const visaIcon = document.getElementById("pan-icon-visa");
-const mcIcon = document.getElementById("pan-icon-mc");
+const visaIcon = document.getElementById('pan-icon-visa');
+const mcIcon = document.getElementById('pan-icon-mc');
 
-const cardNumberInput = document.getElementById("pan");
+const cardNumberInput = document.getElementById('pan');
 const cardNumberInputWrapper = document.querySelector(
-  ".card_info_input_wrapper"
+  '.card_info_input_wrapper'
 );
 
-const expDateInput = document.getElementById("expDate");
-const expDateInputWrapper = document.getElementById("expDateWrapper");
+const expDateInput = document.getElementById('expDate');
+const expDateInputWrapper = document.getElementById('expDateWrapper');
 
-const cvvInput = document.getElementById("cardCvv");
-const cvvInputWrapper = document.getElementById("cvvWrapper");
+const cvvInput = document.getElementById('cardCvv');
+const cvvInputWrapper = document.getElementById('cvvWrapper');
 
 // Close modal functionality
-const handleClose = () => modal.classList.add("hidden");
+const handleClose = () => modal.classList.add('hidden');
 
 // Utility function to format prices to 2 decimal places
 const formatToFixed = (items) => {
@@ -28,7 +28,7 @@ const formatToFixed = (items) => {
   });
 };
 // Format elements
-formatToFixed([".amount", ".btn_amount", ".check_amount"]);
+formatToFixed(['.amount', '.btn_amount', '.check_amount']);
 
 // Determine card type
 const getCardType = (cardNumber) => {
@@ -43,7 +43,7 @@ const getCardType = (cardNumber) => {
 
 // Luhn algorithm for card validation
 function isValidCardNumber(cardNumber) {
-  number = cardNumber.replace(/\s/g, "");
+  number = cardNumber.replace(/\s/g, '');
   let sum = 0;
   let shouldDouble = false;
   for (let i = number.length - 1; i >= 0; i--) {
@@ -61,7 +61,7 @@ function isValidCardNumber(cardNumber) {
 
 // Function to validate expiration date (MM/YY)
 function isValidExpirationDate(expDate) {
-  const [month, year] = expDate.split("/").map(Number);
+  const [month, year] = expDate.split('/').map(Number);
   if (!month || !year || month < 1 || month > 12) return false;
   const currentDate = new Date();
   const inputYear = 2000 + year;
@@ -77,13 +77,13 @@ function isValidExpirationDate(expDate) {
 function handleCardNumber(e) {
   const input = e.target;
   input.value = input.value
-    .replace(/\D/g, "")
-    .replace(/(\d{4})(?=\d)/g, "$1 ")
+    .replace(/\D/g, '')
+    .replace(/(\d{4})(?=\d)/g, '$1 ')
     .trim();
-  const cardType = getCardType(input.value.replace(/\s/g, ""));
+  const cardType = getCardType(input.value.replace(/\s/g, ''));
 
-  visaIcon.classList.toggle("hidden", cardType !== "Visa");
-  mcIcon.classList.toggle("hidden", cardType !== "MasterCard");
+  visaIcon.classList.toggle('hidden', cardType !== 'Visa');
+  mcIcon.classList.toggle('hidden', cardType !== 'MasterCard');
 
   updateButtonState();
 }
@@ -91,16 +91,16 @@ function handleCardNumber(e) {
 // expiry date input format
 
 function handleExpDate(e) {
-  let input = e.target.value.replace(/\D/g, "");
+  let input = e.target.value.replace(/\D/g, '');
   if (
     (input.length === 1 && input[0] > 1 && input[0] <= 9) ||
     (input.length === 2 && input[0] == 1 && input[1] > 2)
   ) {
-    input = "0" + input;
+    input = '0' + input;
   }
 
   if (input.length > 2) {
-    input = input.slice(0, 2) + " / " + input.slice(2, 4);
+    input = input.slice(0, 2) + ' / ' + input.slice(2, 4);
   }
   e.target.value = input.slice(0, 7);
   updateButtonState();
@@ -109,39 +109,39 @@ function handleExpDate(e) {
 // CVC/CVV input format
 
 function handleCvv(e) {
-  let input = e.target.value.replace(/\D/g, "");
+  let input = e.target.value.replace(/\D/g, '');
   e.target.value = input.slice(0, 3);
 
   updateButtonState();
 }
 
 // validate card number on blur event
-cardNumberInput.addEventListener("blur", function () {
+cardNumberInput.addEventListener('blur', function () {
   const cardNumber = cardNumberInput.value;
   // Check if card number is 16 digits and passes Luhn algorithm
   if (!isValidCardNumber(cardNumber)) {
-    cardNumberInputWrapper.classList.add("validation_error");
+    cardNumberInputWrapper.classList.add('validation_error');
   } else {
-    cardNumberInputWrapper.classList.remove("validation_error");
+    cardNumberInputWrapper.classList.remove('validation_error');
   }
 });
 
 // Validate expiration date on blur
-expDateInput.addEventListener("blur", function () {
+expDateInput.addEventListener('blur', function () {
   const expDate = expDateInput.value;
   if (!isValidExpirationDate(expDate)) {
-    expDateInputWrapper.classList.add("validation_error");
+    expDateInputWrapper.classList.add('validation_error');
   } else {
-    expDateInputWrapper.classList.remove("validation_error");
+    expDateInputWrapper.classList.remove('validation_error');
   }
 });
 // Validate cvv/cvc date on blur
-cvvInput.addEventListener("blur", function () {
+cvvInput.addEventListener('blur', function () {
   const cvv = cvvInput.value;
   if (cvv.length !== 3) {
-    cvvInputWrapper.classList.add("validation_error");
+    cvvInputWrapper.classList.add('validation_error');
   } else {
-    cvvInputWrapper.classList.remove("validation_error");
+    cvvInputWrapper.classList.remove('validation_error');
   }
 });
 
@@ -155,57 +155,67 @@ function updateButtonState() {
     isValidExpirationDate(expDate) &&
     cvv.length === 3;
 
-  const submitButton = document.getElementById("cardInfoSubmitBtn");
-  submitButton.classList.toggle("invalid", !allValid);
+  const submitButton = document.getElementById('cardInfoSubmitBtn');
+  submitButton.classList.toggle('invalid', !allValid);
   submitButton.disabled = !allValid;
 }
 
 // mask primary account number aka pan on the second step
-const panElement = document.querySelector(".pan");
+const panElement = document.querySelector('.pan');
 const maskedPan =
-  "*".repeat(12) + panElement.textContent.replace(/\s+/g, "").slice(12);
+  '*'.repeat(12) + panElement.textContent.replace(/\s+/g, '').slice(12);
 panElement.textContent = maskedPan;
+
+// mask primary account number aka pan on the final step
+const transactionInfoPanElement = document.querySelector(
+  '.transaction_info_pan'
+);
+const maskedFinalPan =
+  transactionInfoPanElement.textContent[0] +
+  '*'.repeat(11) +
+  transactionInfoPanElement.textContent.replace(/\s+/g, '').slice(12);
+transactionInfoPanElement.textContent = maskedFinalPan;
 
 // variables to change on second step
 
-const panWrapper = document.querySelector(".pan_wrapper");
-const merchantLocation = document.querySelector(".merchant_location");
-const cardInfoForm = document.querySelector(".card_info_form");
-const codeForm = document.querySelector(".code_form");
-const codeInput = document.getElementById("code");
+const panWrapper = document.querySelector('.pan_wrapper');
+const merchantLocation = document.querySelector('.merchant_location');
+const cardInfoForm = document.querySelector('.card_info_form');
+const codeForm = document.querySelector('.code_form');
+const codeInput = document.getElementById('code');
 
 function handleCode(e) {
-  let input = e.target.value.replace(/\D/g, "");
+  let input = e.target.value.replace(/\D/g, '');
   e.target.value = input.slice(0, 6);
-  const submitButton = document.getElementById("codeSubmitBtn");
+  const submitButton = document.getElementById('codeSubmitBtn');
   const isCodeValid = e.target.value.length === 6;
 
-  submitButton.classList.toggle("invalid", !isCodeValid);
+  submitButton.classList.toggle('invalid', !isCodeValid);
   submitButton.disabled = !isCodeValid;
 }
 
 const showSecStep = (e) => {
   e.preventDefault();
   // display pan on second step
-  panWrapper.style.display = "flex";
+  panWrapper.style.display = 'flex';
   // hide merchant location on second step
-  merchantLocation.style.display = "none";
+  merchantLocation.style.display = 'none';
   // hide card info form on second step
-  cardInfoForm.style.display = "none";
+  cardInfoForm.style.display = 'none';
   // display code form on second step
-  codeForm.style.display = "flex";
+  codeForm.style.display = 'flex';
 };
 
 // variables to change on third step
 
 const merchantAndAmountContainer = document.querySelector(
-  ".merchant_amount_container"
+  '.merchant_amount_container'
 );
-const successCardContainer = document.querySelector(".success_card_container");
+const successCardContainer = document.querySelector('.success_card_container');
 
 const showThirdStep = (e) => {
   e.preventDefault();
-  merchantAndAmountContainer.style.display = "none";
-  codeForm.style.display = "none";
-  successCardContainer.style.display = "flex";
+  merchantAndAmountContainer.style.display = 'none';
+  codeForm.style.display = 'none';
+  successCardContainer.style.display = 'flex';
 };
